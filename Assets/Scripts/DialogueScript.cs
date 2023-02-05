ebca19e8abc8939a41ea137264da511042fbd549
Ximandiv <119233001+Ximandiv@users.noreply.github.com>
2023-02-05 06:50:27 -0500

    Add files via upload

--- Assets/Scripts/DialogueScript.cs
+++ Assets/Scripts/DialogueScript.cs
@@ -6,21 +6,67 @@ using UnityEngine.UI;
 
 public class DialogueScript : MonoBehaviour
 {
-    [SerializeField] private GameObject message;
+    public SpriteRenderer rend;
+    public GameObject place;
+    public PlayerController PlayerController;
+    public Rigidbody2D rb;
+    public bool isFacingRight;
+    private Animator anim;
 
+    private enum movementState
+    {
+        idle, run
+    }
+    private void Flip()
+    {
+        if (isFacingRight && rb.velocity.x < 0f || !isFacingRight && rb.velocity.x > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector2 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+
+    public void Update()
+    {
+        animationupdate();
+        Flip();
+    }
+
+    public void Start()
+    {
+        rend = rend.GetComponent<SpriteRenderer>();
+        rb = rb.GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
     private void OnTriggerStay2D(Collider2D collider)
     {
-        if(collider.tag == "Player")
+        if(collider.tag == "Player" && PlayerController.isDashing)
         {
-            message.SetActive(true);
+            rend.enabled = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.tag == "Player") 
+        rend.enabled = false;
+    }
+    private void animationupdate()
+    {
+        movementState state2;
+        if(rb.velocity.x > 0f)
         {
-            message.SetActive(false);
+            state2 = movementState.run;
         }
+        else if(rb.velocity.x < 0f)
+        {
+            state2 = movementState.run;
+        }
+        else
+        {
+            state2 = movementState.idle;
+        }
+        anim.SetInteger("state2", (int)state2);
     }
 }
